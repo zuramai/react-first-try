eebd7ae3d5c8f282a8e842b58ef23b0463c0e617
zuramai <saugi.dev@gmail.com>
2023-09-03 13:54:13 +0700

    feat: homepage hero section

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,16 @@
+export default function() {
+    return (
+        <>
+            <div className="page ">
+                <div className="section" id="hero">
+                    <div className="home-page macbook-bg">
+                        <div className="hero-text py-16 text-center">
+                            <h2 className="text-2xl mb-5 font-bold lg:text-6xl text-center text-black">Macbook Air 15"</h2>
+                            <p className="text-xl lg:text-3xl">Impressively big. Impossibly thin</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
