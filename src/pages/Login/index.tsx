d7942900e832df8b7609a286be59845f3d336df7
zuramai <saugi.dev@gmail.com>
2023-09-04 07:27:45 +0700

    feat: post login

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,7 +1,27 @@
+import axios from "axios";
 import Button from "../../components/Button";
 import Card from "../../components/Card";
+import { useState } from "react";
+import Alert from "../../components/Alert";
 
 export default function () {
+    const [error, setError] = useState<string|null>(null)
+    const [username, setUsername] = useState("")
+    const [password, setPassword] = useState("")
+
+    function submitForm(e: SubmitEvent) {
+        e.preventDefault()
+        setError(null)
+        axios.post('/auth/login', {
+            username, password
+        })
+        .then(res => [
+            console.log(res)
+        ]).catch(err => {
+            setError(err.response.data.message)
+        })
+    }
+
     return (
         <>
             <div className="page single py-10">
@@ -13,14 +33,22 @@ export default function () {
                     </div>
                     <div className="section-body">
                         <Card>
-                            <form action="" method="post">
+                            {error && <Alert type="danger">{error}</Alert>}
+
+                            <form action="" method="post" onSubmit={submitForm}>
                                 <div className="input-group mb-3">
                                     <label htmlFor="username">Username</label>
-                                    <input id="username" type="text" className="input"/>
+                                    <input 
+                                        id="username" type="text" className="input" 
+                                        onInput={(e) => setUsername((e.target as HTMLInputElement).value)}
+                                        />
                                 </div>
                                 <div className="input-group mb-3">
                                     <label htmlFor="password">Password</label>
-                                    <input id="password" type="text" className="input"/>
+                                    <input 
+                                        id="password" type="password" className="input" 
+                                        onInput={(e) => setPassword((e.target as HTMLInputElement).value)}
+                                        />
                                 </div>
                                 <Button>Submit</Button>
                             </form>
