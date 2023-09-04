d3a42d9d92b18d7921cd9559cdd16002b6c9c2f1
zuramai <saugi.dev@gmail.com>
2023-09-04 12:35:40 +0700

    chore: add path alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import UnoCSS from 'unocss/vite'
+import path from 'path'
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(),
     UnoCSS(),
   ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, "./src")
+    }
+  }
 })
